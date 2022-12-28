f11268d3ffd0c6d1ffbb53dbab4c3f04e048e36c
patryk <patryk.turkowski21@gmail.com>
2022-12-28 14:26:25 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,25 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('splade')->group(function () {
+Route::middleware(['splade'])->group(function () {
+    Route::get('/', fn () => view('home'))->name('home');
+    Route::get('/docs', fn () => view('docs'))->name('docs');
+
     // Registers routes to support Table Bulk Actions and Exports...
     Route::spladeTable();
 
     // Registers routes to support async File Uploads with Filepond...
     Route::spladeUploads();
-
-
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/', DashboardController::class)->name('dashboard');
-        Route::get('/dashboard', DashboardController::class)->middleware(['verified'])->name('dashboard');
-
-        Route::resource('users', UserController::class);
-
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
-    require __DIR__ . '/auth.php';
 });
